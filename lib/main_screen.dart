607f50de9c418cb3e73855e46a5a48b203684a85
heebmohammad <157803874+heebm@users.noreply.github.com>
2024-06-25 12:37:31 +0300

    solve 1

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -5,11 +5,35 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    IconAlignment _iconAlignment = IconAlignment.start;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Main Screen"),
       ),
-      body: const Text("Hello Flutter"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Hello Flutter"),
+            SizedBox(height: 20), // Adding some space between text and button
+            ElevatedButton(
+              onPressed: () {
+                // Button onPressed action
+              },
+
+              child: Text("My Button"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.),
+              label: const Text('ElevatedButton'),
+              iconAlignment: _iconAlignment,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
